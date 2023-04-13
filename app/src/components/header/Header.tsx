d119080d2fa6983bd48350dbd2ace764e79215a3
Paulo Nakata <paulonkt@hotmail.com>
2023-04-13 19:15:08 +0100

    New Header component added

--- app/src/components/header/Header.tsx
+++ app/src/components/header/Header.tsx
@@ -1,10 +1,28 @@
-import React from 'react';
+import Button from '../common/button/Button';
+import working from '../../assets/illustration-working.svg';
 import './header.css';
 
 const Header = () => {
 	return (
-		<div>
-			Header
+		<div className='sep__header' id="home">
+			<div className="sep__header-content">
+				<div className="sep__header-texts">
+					<h1>
+						More than just<br/>
+						shorter links
+					</h1>
+					<p>
+						Build your brand's recognition and get detailed
+						insights on how your links are performing.
+					</p>
+					<div className='sep__header-button_center'>
+						<Button text="Get Started" height={40} width={140} />
+					</div>
+				</div>
+				<div className="sep__header-image">
+					<img src={working} />
+				</div>
+			</div>
 		</div>
 	)
 }
