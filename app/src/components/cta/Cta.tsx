8b9493d52c94c017663cf7903251e19814bcd0e7
Paulo Nakata <paulonkt@hotmail.com>
2023-04-13 19:33:42 +0100

    New Cta Component added

--- app/src/components/cta/Cta.tsx
+++ app/src/components/cta/Cta.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import './cta.css';
+import Button from '../common/button/Button';
 
 const Cta = () => {
 	return (
-		<div>
-			Cta
+		<div className='sep__cta'>
+			<div className="sep__cta-call">
+				<h1>Boost your links today</h1>
+			</div>
+			<div className='sep__cta-button_center'>
+				<Button text="Get Started" height={40} width={140} />
+			</div>
 		</div>
 	)
 }
