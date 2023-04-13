8961c7e963356ba1bd3b9342c854d95198ce6893
Paulo Nakata <paulonkt@hotmail.com>
2023-04-13 21:25:23 +0100

    Changed tag h1 to h3 in Cta component

--- app/src/components/cta/Cta.tsx
+++ app/src/components/cta/Cta.tsx
@@ -6,7 +6,7 @@ const Cta = () => {
 	return (
 		<div className='sep__cta'>
 			<div className="sep__cta-call">
-				<h1>Boost your links today</h1>
+				<h3>Boost your links today</h3>
 			</div>
 			<div className='sep__cta-button_center'>
 				<Button text="Get Started" height={40} width={140} />
