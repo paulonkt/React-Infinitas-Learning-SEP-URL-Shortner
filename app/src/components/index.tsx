4094e8c9124c6f336f845b2bc0f6a22589b8264f
Paulo Nakata <paulonkt@hotmail.com>
2023-04-13 18:39:52 +0100

    Button and Logo components added to index

--- app/src/components/index.tsx
+++ app/src/components/index.tsx
@@ -4,4 +4,6 @@ export { default as Feature } from './feature/Feature';
 export { default as Statistics } from './statistics/Statistics';
 export { default as Cta } from './cta/Cta';
 export { default as Footer } from './footer/Footer';
-export { default as Button } from './button/Button';
+
+export { default as Button } from './common/button/Button';
+export { default as Logo } from './common/logo/Logo';
